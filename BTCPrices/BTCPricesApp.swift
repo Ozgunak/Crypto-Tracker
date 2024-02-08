329fa704d214e0c8dd8a4683add2492d16b50f49
Ozgun Aksoy <zgnaksoy@gmail.com>
2024-02-08 18:12:07 +0300

    Initial Commit

---
+++ BTCPrices/BTCPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCPricesApp.swift
+//  BTCPrices
+//
+//  Created by özgün aksoy on 2024-02-08.
+//
+
+import SwiftUI
+
+@main
+struct BTCPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
